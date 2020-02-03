ada046fe67f9ed455ed7289b478b270105a59889
Gram <master_fess@mail.ru>
2020-02-03 12:34:22 +0100

    +all doc props

--- document.go
+++ document.go
@@ -31,6 +31,10 @@ func (doc *Document) CharacterSet() string {
 	return doc.window.Get("characterSet").String()
 }
 
+func (doc *Document) CompatMode() string {
+	return doc.window.Get("compatMode").String()
+}
+
 // ContentType returns document's content type.
 func (doc *Document) ContentType() string {
 	return doc.window.Get("contentType").String()
@@ -48,11 +52,27 @@ func (doc *Document) Domain() string {
 	}
 }
 
+func (doc *Document) Referrer() string {
+	return doc.window.Get("referrer").String()
+}
+
 func (doc *Document) InputEncoding() string {
 	return doc.window.Get("inputEncoding").String()
 }
 
+func (doc *Document) ReadyState() string {
+	return doc.window.Get("readyState").String()
+}
+
+func (doc *Document) Title() string {
+	return doc.window.Get("title").String()
+}
+
 // DOCUMENT NON-STRING PROPERTIES
+
+func (doc *Document) ChildElementCount() int {
+	return doc.window.Get("childElementCount").Int()
+}
 
 // DesignMode indicates whether the document can be edited.
 func (doc *Document) DesignMode() bool {
@@ -63,8 +83,16 @@ func (doc *Document) FullscreenEnabled() bool {
 	return doc.window.Get("fullscreenEnabled").Bool()
 }
 
+func (doc *Document) Hidden() bool {
+	return doc.window.Get("hidden").Bool()
+}
+
 func (doc *Document) LastModified() time.Time {
 	date := doc.window.Get("lastModified").String()
 	timestamp := doc.window.Get("Date").Call("parse", date).Float()
 	return time.Unix(0, int64(timestamp))
+}
+
+func (doc *Document) XMLStandalone() bool {
+	return doc.window.Get("xmlStandalone").Bool()
 }
