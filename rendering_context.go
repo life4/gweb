d4b8dc9c6d2ec7d80dd75b24e634bebb75025c05
Gram <master_fess@mail.ru>
2020-02-03 17:16:47 +0100

    setters

--- rendering_context.go
+++ rendering_context.go
@@ -168,16 +168,32 @@ func (context *Shadow) Blur() float64 {
 	return context.value.Get("shadowBlur").Float()
 }
 
+func (context *Shadow) SetBlur(value float64) {
+	context.value.Set("shadowBlur", value)
+}
+
 func (context *Shadow) Color() string {
 	return context.value.Get("shadowColor").String()
+}
+
+func (context *Shadow) SetColor(value string) {
+	context.value.Set("shadowColor", value)
 }
 
 func (context *Shadow) OffsetX() float64 {
 	return context.value.Get("shadowOffsetX").Float()
 }
 
+func (context *Shadow) SetOffsetX(value float64) {
+	context.value.Set("shadowOffsetX", value)
+}
+
 func (context *Shadow) OffsetY() float64 {
 	return context.value.Get("shadowOffsetY").Float()
+}
+
+func (context *Shadow) SetOffsetY(value float64) {
+	context.value.Set("shadowOffsetY", value)
 }
 
 type Line struct {
@@ -188,16 +204,39 @@ func (context *Line) Cap() string {
 	return context.value.Get("lineCap").String()
 }
 
+func (context *Line) SetCap(value string) {
+	context.value.Set("lineCap", value)
+}
+
 func (context *Line) Join() string {
 	return context.value.Get("lineJoin").String()
+}
+
+func (context *Line) SetJoin(value string) {
+	context.value.Set("lineJoin", value)
 }
 
 func (context *Line) MiterLimit() string {
 	return context.value.Get("miterLimit").String()
 }
 
+func (context *Line) SetMiterLimit(value string) {
+	context.value.Set("miterLimit", value)
+}
+
 func (context *Line) Width() int {
 	return context.value.Get("lineWidth").Int()
+}
+
+func (context *Line) SetWidth(value int) {
+	context.value.Set("lineWidth", value)
+}
+
+func (context *Line) Draw(x1, y1, x2, y2 int) {
+	context.value.Call("beginPath")
+	context.value.Call("moveTo", x1, y1)
+	context.value.Call("lineTo", x2, y2)
+	context.value.Call("stroke")
 }
 
 type Text struct {
