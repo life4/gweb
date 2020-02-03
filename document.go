736dbf6d0e4349fd309f71c6ef20408caadfd5c4
Gram <master_fess@mail.ru>
2020-02-03 20:29:57 +0100

    convert Document into js.Value

--- document.go
+++ document.go
@@ -6,95 +6,95 @@ import (
 )
 
 type Document struct {
-	value js.Value
+	js.Value
 }
 
 func GetDocument() Document {
 	window := js.Global()
-	return Document{value: window.Get("document")}
+	return Document{Value: window.Get("document")}
 }
 
 // DOCUMENT STRING PROPERTIES
 
 // URL returns the URL for the current document.
 func (doc *Document) URL() string {
-	return doc.value.Get("URL").String()
+	return doc.Get("URL").String()
 }
 
 // Cookie returns the HTTP cookies that apply to the Document.
 // If there are no cookies or cookies can't be applied to this resource, the empty string will be returned.
 func (doc *Document) Cookie() string {
-	return doc.value.Get("URL").String()
+	return doc.Get("URL").String()
 }
 
 // CharacterSet returns document's encoding.
 func (doc *Document) CharacterSet() string {
-	return doc.value.Get("characterSet").String()
+	return doc.Get("characterSet").String()
 }
 
 func (doc *Document) CompatMode() string {
-	return doc.value.Get("compatMode").String()
+	return doc.Get("compatMode").String()
 }
 
 // ContentType returns document's content type.
 func (doc *Document) ContentType() string {
-	return doc.value.Get("contentType").String()
+	return doc.Get("contentType").String()
 }
 
 func (doc *Document) Domain() string {
-	v := doc.value.Get("domain")
+	v := doc.Get("domain")
 	return Value{Value: v}.OptionalString()
 }
 
 func (doc *Document) Referrer() string {
-	return doc.value.Get("referrer").String()
+	return doc.Get("referrer").String()
 }
 
 func (doc *Document) InputEncoding() string {
-	return doc.value.Get("inputEncoding").String()
+	return doc.Get("inputEncoding").String()
 }
 
 func (doc *Document) ReadyState() string {
-	return doc.value.Get("readyState").String()
+	return doc.Get("readyState").String()
 }
 
 func (doc *Document) Title() string {
-	return doc.value.Get("title").String()
+	return doc.Get("title").String()
 }
 
 // DOCUMENT NON-STRING PROPERTIES
 
 func (doc *Document) ChildElementCount() int {
-	return doc.value.Get("childElementCount").Int()
+	return doc.Get("childElementCount").Int()
 }
 
 // DesignMode indicates whether the document can be edited.
 func (doc *Document) DesignMode() bool {
-	return doc.value.Get("designMode").String() == "on"
+	return doc.Get("designMode").String() == "on"
 }
 
 func (doc *Document) FullscreenEnabled() bool {
-	return doc.value.Get("fullscreenEnabled").Bool()
+	return doc.Get("fullscreenEnabled").Bool()
 }
 
 func (doc *Document) Hidden() bool {
-	return doc.value.Get("hidden").Bool()
+	return doc.Get("hidden").Bool()
 }
 
 func (doc *Document) LastModified() time.Time {
-	date := doc.value.Get("lastModified").String()
-	timestamp := doc.value.Get("Date").Call("parse", date).Float()
+	date := doc.Get("lastModified").String()
+	timestamp := doc.Get("Date").Call("parse", date).Float()
 	return time.Unix(0, int64(timestamp))
 }
 
 func (doc *Document) XMLStandalone() bool {
-	return doc.value.Get("xmlStandalone").Bool()
+	return doc.Get("xmlStandalone").Bool()
 }
 
 // JS FUNCS
 
 func (doc *Document) CreateElement(name string) Value {
-	value := doc.value.Call("createElement", name)
+	value := doc.Call("createElement", name)
 	return Value{Value: value}
 }
 
