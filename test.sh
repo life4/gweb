bef9e20ee3f4fb81b5da4a1ceb11b862a17f06fb
Gram <master_fess@mail.ru>
2020-02-15 09:30:48 +0100

    smoke test examples

--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+set -e
+
 GOOS=js GOARCH=wasm go test -exec=wasmbrowsertest ./web/
+
+examples=( "draw" "events" "hello" "pacman" "server" "styling" "templates" "triangle" )
+
+for name in "${examples[@]}"
+do
+    echo "compiling $name..."
+    GOOS=js GOARCH=wasm go build -o /tmp/bin "./examples/$name/"
+done
+echo "done"
