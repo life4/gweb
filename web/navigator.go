0327a3d072b32f74c1091ea9bf00512db7f809bc
Gram <master_fess@mail.ru>
2020-02-17 16:26:37 +0100

    improve navigator a bit

--- web/navigator.go
+++ web/navigator.go
@@ -6,26 +6,37 @@ type Navigator struct {
 
 // PROPERTIES
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/cookieEnabled
 func (nav Navigator) CookieEnabled() bool {
 	return nav.Get("cookieEnabled").Bool()
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/NavigatorLanguage/language
 func (nav Navigator) Language() string {
 	return nav.Get("language").String()
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/NavigatorLanguage/languages
 func (nav Navigator) Languages() []string {
 	return nav.Get("languages").Strings()
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/maxTouchPoints
 func (nav Navigator) MaxTouchPoints() int {
 	return nav.Get("maxTouchPoints").Int()
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/mediaDevices
+func (nav Navigator) MediaDevices() MediaDevices {
+	return MediaDevices{Value: nav.Get("mediaDevices")}
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/onLine
 func (nav Navigator) Online() bool {
 	return nav.Get("onLine").Bool()
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/NavigatorID/userAgent
 func (nav Navigator) UserAgent() string {
 	return nav.Get("userAgent").String()
 }
