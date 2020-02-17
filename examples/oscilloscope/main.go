d3c1090314e4f0b083a47c5308242a7eb5caf7c6
Gram <master_fess@mail.ru>
2020-02-17 15:41:31 +0100

    draw oscilloscope

---
+++ examples/oscilloscope/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/life4/gweb/audio"
+
+	"github.com/life4/gweb/canvas"
+	"github.com/life4/gweb/web"
+)
+
+const BGColor = "#ecf0f1"
+const LineColor = "#2c3e50"
+
+type Scope struct {
+	domain  audio.TimeDomainBytes
+	context canvas.Context2D
+	width   int
+	height  int
+}
+
+func (scope *Scope) handle() {
+	chunkWidth := scope.width / scope.domain.Size
+
+	scope.context.SetFillStyle(BGColor)
+	scope.context.Rectangle(0, 0, scope.width, scope.height).Filled().Draw()
+	scope.context.MoveTo(0, scope.height/2)
+
+	scope.context.SetFillStyle(LineColor)
+	scope.domain.Update()
+	x := 0
+	for _, freq := range scope.domain.Data {
+		y := int(freq) * scope.height / 256
+		scope.context.LineTo(x, y)
+		x += chunkWidth
+	}
+	scope.context.LineTo(scope.width, scope.height/2)
+	scope.context.Stroke()
+}
+
+func main() {
+	window := web.GetWindow()
+	doc := window.Document()
+	doc.SetTitle("Canvas drawing example")
+	body := doc.Body()
+
+	// create canvas
+	h := window.InnerHeight() - 40
+	w := window.InnerWidth() - 40
+	canvas := doc.CreateCanvas()
+	canvas.SetHeight(h)
+	canvas.SetWidth(w)
+	body.Node().AppendChild(canvas.Node())
+
+	context := canvas.Context2D()
+
+	// draw background
+	context.SetFillStyle(BGColor)
+	context.Rectangle(0, 0, w, h).Filled().Draw()
+
+	audio := window.AudioContext()
+	analyser := audio.Analyser()
+	domain := analyser.TimeDomain()
+
+	// register handlers
+	scope := Scope{
+		domain:  domain,
+		context: context,
+		width:   w,
+		height:  h,
+	}
+	window.RequestAnimationFrame(scope.handle, true)
+	// prevent ending of the program
+	select {}
+}
