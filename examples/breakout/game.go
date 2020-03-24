5dd2159d2c902d9e6658b1cb2a9623caa51032a3
Gram <master_fess@mail.ru>
2020-03-24 11:18:29 +0100

    incapsulate game logic

---
+++ examples/breakout/game.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"sync"
+	"time"
+
+	"github.com/life4/gweb/web"
+)
+
+type Game struct {
+	Width  int
+	Height int
+	Window web.Window
+	Canvas web.Canvas
+	Body   web.HTMLElement
+
+	platform Platform
+	ball     Ball
+	block    TextBlock
+	bricks   Bricks
+}
+
+func (game *Game) Init() {
+	context := game.Canvas.Context2D()
+
+	// draw background
+	context.SetFillStyle(BGColor)
+	context.BeginPath()
+	context.Rectangle(0, 0, game.Width, game.Height).Filled().Draw()
+	context.Fill()
+	context.ClosePath()
+
+	// make handlers
+	rect := Rectangle{
+		x:      game.Width / 2,
+		y:      game.Height - 60,
+		width:  PlatformWidth,
+		height: PlatformHeight,
+	}
+	platformCicrle := CircleFromRectangle(rect)
+	game.platform = Platform{
+		rect:         &rect,
+		circle:       &platformCicrle,
+		context:      context,
+		element:      game.Canvas,
+		mouseX:       game.Width / 2,
+		windowWidth:  game.Width,
+		windowHeight: game.Height,
+	}
+	game.block = TextBlock{context: context, updated: time.Now()}
+	ballCircle := Circle{
+		x:      game.platform.circle.x,
+		y:      game.platform.rect.y - BallSize - 5,
+		radius: BallSize,
+	}
+	game.ball = Ball{
+		context:      context,
+		vector:       Vector{x: 5, y: -5},
+		Circle:       ballCircle,
+		windowWidth:  game.Width,
+		windowHeight: game.Height,
+		platform:     &game.platform,
+	}
+	game.bricks = Bricks{
+		context:      context,
+		windowWidth:  game.Width,
+		windowHeight: game.Height,
+		ready:        false,
+		text:         &game.block,
+	}
+	go game.bricks.Draw()
+}
+
+func (game *Game) handler() {
+	wg := sync.WaitGroup{}
+	wg.Add(4)
+	go func() {
+		// update FPS
+		game.block.handle()
+		wg.Done()
+	}()
+	go func() {
+		// update platform position
+		game.platform.handleFrame()
+		wg.Done()
+	}()
+	go func() {
+		// check if the ball should bounce from a brick
+		game.bricks.Handle(&game.ball)
+		wg.Done()
+	}()
+	go func() {
+		// check if the ball should bounce from border or platform
+		game.ball.handle()
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func (game *Game) Register() {
+	// register mouse movement handler
+	game.Body.EventTarget().Listen(web.EventTypeMouseMove, game.platform.handleMouse)
+	// register frame updaters
+	game.Window.RequestAnimationFrame(game.handler, true)
+}
--- examples/breakout/main.go
+++ examples/breakout/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"sync"
-	"time"
-
 	"github.com/life4/gweb/web"
 )
 
@@ -21,84 +18,15 @@ func main() {
 	canvas.SetWidth(w)
 	body.Node().AppendChild(canvas.Node())
 
-	context := canvas.Context2D()
-
-	// draw background
-	context.SetFillStyle(BGColor)
-	context.BeginPath()
-	context.Rectangle(0, 0, w, h).Filled().Draw()
-	context.Fill()
-	context.ClosePath()
-
-	// make handlers
-	rect := Rectangle{
-		x:      w / 2,
-		y:      h - 60,
-		width:  PlatformWidth,
-		height: PlatformHeight,
+	game := Game{
+		Width:  w,
+		Height: h,
+		Window: window,
+		Canvas: canvas,
+		Body:   body,
 	}
-	platformCicrle := CircleFromRectangle(rect)
-	platform := Platform{
-		rect:         &rect,
-		circle:       &platformCicrle,
-		context:      context,
-		element:      canvas,
-		mouseX:       w / 2,
-		windowWidth:  w,
-		windowHeight: h,
-	}
-	block := TextBlock{context: context, updated: time.Now()}
-	ballCircle := Circle{
-		x:      platform.circle.x,
-		y:      platform.rect.y - BallSize - 5,
-		radius: BallSize,
-	}
-	ball := Ball{
-		context:     context,
-		vector:      Vector{x: 5, y: -5},
-		Circle:      ballCircle,
-		windowWidth: w, windowHeight: h,
-		platform: &platform,
-	}
-	bricks := Bricks{
-		context:      context,
-		windowWidth:  w,
-		windowHeight: h,
-		ready:        false,
-		text:         &block,
-	}
-	go bricks.Draw()
-
-	// register mouse movement handler
-	body.EventTarget().Listen(web.EventTypeMouseMove, platform.handleMouse)
-
-	// register frame updaters
-	handler := func() {
-		wg := sync.WaitGroup{}
-		wg.Add(4)
-		go func() {
-			// update FPS
-			block.handle()
-			wg.Done()
-		}()
-		go func() {
-			// update platform position
-			platform.handleFrame()
-			wg.Done()
-		}()
-		go func() {
-			// check if the ball should bounce from a brick
-			bricks.Handle(&ball)
-			wg.Done()
-		}()
-		go func() {
-			// check if the ball should bounce from border or platform
-			ball.handle()
-			wg.Done()
-		}()
-		wg.Wait()
-	}
-	window.RequestAnimationFrame(handler, true)
+	game.Init()
+	game.Register()
 
 	// prevent ending of the program
 	select {}
