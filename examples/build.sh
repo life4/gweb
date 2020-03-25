4ccf2f0f0cb55231c7853a9e5238a3112332b0ef
Gram <master_fess@mail.ru>
2020-03-25 14:39:36 +0100

    split build and run

---
+++ examples/build.sh
@@ -0,0 +1,5 @@
+set -e
+mkdir -p build
+cp ./frontend/* ./build/
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./build/script.js
+GOOS=js GOARCH=wasm go build -o build/frontend.wasm ./$1/
--- examples/run.sh
+++ examples/run.sh
@@ -1,7 +1,4 @@
 set -e
-mkdir -p build
-cp ./frontend/* ./build/
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./build/script.js
-GOOS=js GOARCH=wasm go build -o build/frontend.wasm ./$1/
+./build.sh $1
 go build -o server.bin ./server/
 ./server.bin
