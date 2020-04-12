7f05c7898f321cfe01584164effb0ad3e740323a
Gram <master_fess@mail.ru>
2020-04-12 13:16:53 +0200

    +links 4 console

--- web/console.go
+++ web/console.go
@@ -1,5 +1,6 @@
 package web
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console
 type Console struct {
 	Value
 }
@@ -14,22 +15,27 @@ func (console Console) log(fname, format string, args []interface{}) {
 	}
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/log
 func (console Console) Log(format string, args ...interface{}) {
 	console.log("log", format, args)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/debug
 func (console Console) Debug(format string, args ...interface{}) {
 	console.log("debug", format, args)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/info
 func (console Console) Info(format string, args ...interface{}) {
 	console.log("info", format, args)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/warn
 func (console Console) Warning(format string, args ...interface{}) {
 	console.log("warn", format, args)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/error
 func (console Console) Error(format string, args ...interface{}) {
 	console.log("error", format, args)
 }
@@ -44,50 +50,62 @@ func (console Console) callWithLabel(fname, label string) {
 	}
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/clear
 func (console Console) Clear() {
 	console.Call("clear")
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/count
 func (console Console) Count(label string) {
 	console.callWithLabel("count", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/countReset
 func (console Console) CountReset(label string) {
 	console.callWithLabel("countReset", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/group
 func (console Console) Group(label string) {
 	console.callWithLabel("group", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/groupCollapsed
 func (console Console) GroupCollapsed(label string) {
 	console.callWithLabel("groupCollapsed", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/groupEnd
 func (console Console) GroupEnd() {
 	console.Call("groupEnd")
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/profile
 func (console Console) Profile(label string) {
 	console.callWithLabel("profile", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/profileEnd
 func (console Console) ProfileEnd(label string) {
 	console.callWithLabel("profileEnd", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/time
 func (console Console) Time(label string) {
 	console.callWithLabel("time", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/timeEnd
 func (console Console) TimeEnd(label string) {
 	console.callWithLabel("timeEnd", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/timeLog
 func (console Console) TimeLog(label string) {
 	console.callWithLabel("timeLog", label)
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Console/trace
 func (console Console) Trace(args ...interface{}) {
 	console.Call("trace", args...)
 }
